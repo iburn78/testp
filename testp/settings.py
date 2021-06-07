d678c41515a3c20d274f5ffe34eeb313476e2b58
Andy Kim <andy.chiwon.kim@gmail.com>
2021-06-07 17:07:34 +0900

    wip

--- testp/settings.py
+++ testp/settings.py
@@ -136,3 +136,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "issuetree.tracker@gmail.com"
+EMAIL_HOST_PASSWORD = "xiaphkrwtlfggttw"
